d5f8d86dc44b3207073b100870e637e04e635d94
Marc Frankfurter <johndoe@example.commarcfrankfurter@gmail.com>
2024-07-21 16:37:23 +0200

    fixing deployment and adding index page

--- pages/game.vue
+++ pages/game.vue
@@ -1,10 +1,10 @@
+<script setup>
+import ChessTacToe from "../components/ChessTacToe.vue";
+</script>
+
 <template>
   <div>
     <h2 class="text-center p-8">Chess Tic Tac Toe Game</h2>
     <ChessTacToe />
   </div>
 </template>
-
-<script setup>
-import ChessTacToe from "../components/ChessTacToe.vue";
-</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Welcome to Nuxt Chess Tic-Tac-Toe</h1>
+    <p>This is the index page.</p>
+    <nuxt-link to="/game">Go to Game</nuxt-link>
+  </div>
+</template>
+
+<style scoped></style>
